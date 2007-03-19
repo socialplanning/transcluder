5f3cb64bb7810fc05fb1eeea75b48ed429663ef6
David Turner <novalis@novalis.org>
2007-03-19 21:50:50 +0000

    removed debug lock
    git-svn-id: https://svn.socialplanning.org/svn/transcluder/trunk@3692 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- transcluder/locked.py
+++ transcluder/locked.py
@@ -11,7 +11,7 @@ def locked(func, *args, **kw):
     return result
 
 @decorator
-def locked(func, *args, **kw):
+def xlocked(func, *args, **kw):
     if hasattr(args[0], 'cv'):
         lock = args[0].cv
     else:
