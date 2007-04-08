f50d304fce970f450a5318dadc07237a07f7e06f
Luke Tucker <voxluci@gmail.com>
2007-04-08 03:03:41 +0000

    use wsgifilter forced proxy in test app
    git-svn-id: https://svn.socialplanning.org/svn/transcluder/trunk@4093 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- transcluder/proxyapp.py
+++ transcluder/proxyapp.py
@@ -1,8 +1,8 @@
-from paste.proxy import TransparentProxy 
+from wsgifilter.proxyapp import ForcedProxy
 from transcluder.middleware import TranscluderMiddleware
 
 
 def make_proxy(global_conf, **app_conf):
-    app = TransparentProxy(force_host=app_conf.get('force_host'))
+    app = ForcedProxy(remote=app_conf.get('force_host'))
     app = TranscluderMiddleware(app)
     return app
