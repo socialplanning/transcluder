91859b5b7fff1020b95488fb09561f0cdd8c561c
David Turner <novalis@novalis.org>
2008-03-24 21:21:55 +0000

    oops
    git-svn-id: https://svn.socialplanning.org/svn/transcluder/trunk@14636 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- transcluder/transclude.py
+++ transcluder/transclude.py
@@ -106,13 +106,11 @@ class Transcluder:
                 else:
                     self.merge(target, subdoc, source_url)
 
-            except: 
-                print "HERE"
 
-#             except object, message:
-#                 print "HERE", message
-#                 self.attach_warning(target, "Failed to retrieve (%s), url: %s"
-#                                     % (message, source_url))
+             except object, message:
+                 print "HERE", message
+                 self.attach_warning(target, "Failed to retrieve (%s), url: %s"
+                                     % (message, source_url))
                 # XXX should log traceback.format_exc() ?  
         return True
 
